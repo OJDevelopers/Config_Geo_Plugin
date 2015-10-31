f64e28d126985a5d09090c88f8b44145b14c6258
Luis Eduardo Oquendo Perez <leop482@hotmail.com>
2015-10-30 19:12:05 -0500

    settings

--- src/android/config_geo_plugin.java
+++ src/android/config_geo_plugin.java
@@ -73,8 +73,8 @@ public class config_geo_plugin extends CordovaPlugin {
      */
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("open")) {
-            Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            context.startActivity(i); 
+            Intent i = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(i); 
         }
         else {
             return false;
